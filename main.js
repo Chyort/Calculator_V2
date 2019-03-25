c20e9c80ecf3ff07001c6b090a586b18fa8ea61a
Lou Pruitt <Lpruitt2004@gmail.com>
2019-03-24 18:21:30 -0700

    Add operators to display

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ class Calculator {
         this.initialize = this.initialize.bind(this);
     }
 
-    //jQuery selectors on event will call method
+    //jQuery selectors on event will call method.
     initialize(){
         $('.number').click(this.handleNumber);
         $('.clear').click(this.clearAll);
-        
+        $('.operator').click(this.handleOperator);
     }
 
-    //Display inputArray on .calculatorScreen
+    //Display inputArray on .calculatorScreen.
     display() {
         var result = calc.inputArray.join(" ");
         if (calc.inputArray.length === 0) {
@@ -33,7 +33,8 @@ class Calculator {
         console.log('clearAll executed');
     }
 
-    //First input is turned into a string and pushed to inputArray, successive inputs are parameters for concatLastInput
+    //First input is turned into a string and pushed to inputArray,
+    //successive inputs are parameters for concatLastInput.
     handleNumber(input) {
         calc.input = input;
 
@@ -50,18 +51,45 @@ class Calculator {
 
     }
 
-    //Returns the inputArray and checks if last index in inputArray is a number
+    //Returns the inputArray and checks if last index in inputArray is a number.
     isLastInputNum(inputArray) {
         console.log('isLastInputNum executed');
 
         return inputArray && !isNaN(inputArray[inputArray.length - 1]);
     }
 
-    //Takes the inputArray that has at least one number and concatenates value(input)
+    //Takes the inputArray that has at least one number and concatenates value(input).
     concatLastInput(inputArray, value) {
         inputArray[inputArray.length - 1] += value;
 
         console.log('concatLastInput executed');
+    }
+
+    // Takes the input and pushes it to inputArray if there is a previous
+    // input that is a number in inputArray, replaces the last inputArray input if it is a number.
+    handleOperator(input) {
+        if (calc.inputArray.length < 1) {
+            return;
+        }
+        if (typeof input != 'string') {
+            input = $(this).text();
+            calc.input = input;
+        }
+        if (calc.isLastInputNum(calc.inputArray)) {
+            calc.inputArray.push(input);
+        } else {
+            calc.replaceLastInput(calc.inputArray, calc.input);
+        }
+        calc.display();
+
+        console.log('handleOperator executed');
+    }
+
+    //Replaces last index in inputArray with new value.
+    replaceLastInput(inputArray, value) {
+        inputArray[inputArray.length - 1] = value;
+
+        console.log('replaceLastInput executed');
     }
 
 }
